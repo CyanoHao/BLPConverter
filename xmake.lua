1eb3cd9e2576ba2ad49164a40edc9b2cac26de2c
Cyano Hao <c@cyano.cn>
2024-07-25 15:46:43 +0800

    Use VC-LTL on Windows

--- xmake.lua
+++ xmake.lua
@@ -8,9 +8,16 @@ add_requires(
     "libsquish ^1.15",
     "nowide_standalone ^11.3.0")
 
+if is_plat("windows") then
+    add_requires("vc-ltl5 ^5.0.9", {configs = {min_version = "10.0.10240"}})
+end
+
 target("BLPConverter")
     set_kind("binary")
     add_packages("cli11", "fmt", "freeimage", "nowide_standalone")
+    if is_plat("windows") then
+        add_packages("vc-ltl5")
+    end
 
     add_files("src/*.cpp")
     add_deps("blp")
